dc83ebbec6e279d3c142fdce147e19d7ff7f2bd0
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-04-23 15:13:50 +0000

    update

---
+++ greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "coderCat";
+document.body.textContent = greeter(user);
---
+++ greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string): string {
+    return "Hello, " + person;
+}
+
+let user = "coderMan";
+
+console.log(greeter(user)); // Print the output to the terminal
